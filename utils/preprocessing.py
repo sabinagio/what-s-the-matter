7ae705b23ad1546b4527fd7b280e2c2aa4d06df4
sabinagio <sabinagiorgiana@gmail.com>
2022-09-18 14:39:21 +0300

    Add emoji translation & renew poetry packages

--- utils/preprocessing.py
+++ utils/preprocessing.py
@@ -1,5 +1,6 @@
 import re
 import string
+import emoji
 from textblob import TextBlob #spelling correction
 import nltk.pos_tag
 from nltk.corpus import stopwords, wordnet
@@ -86,3 +87,36 @@ def lemmatization(text):
     lemmatizer = WordNetLemmatizer()
     lemmatized = [lemmatizer.lemmatize(pair[0], pos=get_type(pair[1])) for pair in word_and_tag]
     return ' '.join(lemmatized)
+
+def translate_emojis(df, text_col="text"):
+
+        # Define emoji patterns
+        pattern = re.compile(pattern = "["
+                u"\U0001F600-\U0001F64F" 
+                u"\U0001F300-\U0001F5FF"
+                u"\U0001F680-\U0001F6FF"
+                u"\U0001F1E0-\U0001F1FF"
+                u"\U00002500-\U00002BEF"
+                u"\U00002702-\U000027B0"
+                u"\U00002702-\U000027B0"
+                u"\U000024C2-\U0001F251"
+                u"\U0001f926-\U0001f937"
+                u"\U00010000-\U0010ffff"
+                u"\u2640-\u2642" 
+                u"\u2600-\u2B55"
+                u"\u200d"
+                u"\u23cf"
+                u"\u23e9"
+                u"\u231a"
+                u"\ufe0f"
+                u"\u3030"
+                                   "]+", flags = re.UNICODE)
+
+        # Return emojis found and translate each into a Pandas Series
+        matches = pd.Series(map(lambda x: pattern.findall(x), df['text']))
+        matches = matches.apply(lambda x: "".join(x))
+        matches = matches.apply(lambda x: emoji.demojize(x))
+        matches = matches.apply(lambda x: np.nan if x == "" else x)
+
+        # Append translated emojis to dataframe
+        df["emojis"] = matches
